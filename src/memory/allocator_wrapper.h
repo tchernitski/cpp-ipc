b7656870e191ebd9a0125043336309169894329d
amba <vtchernitski@gmail.com>
2019-10-31 14:20:29 +0300

    fixed compilation error

--- src/memory/allocator_wrapper.h
+++ src/memory/allocator_wrapper.h
@@ -56,6 +56,7 @@ public:
     template <typename U>
     allocator_wrapper(const allocator_wrapper<U, AllocP>&) noexcept {}
 
+    allocator_wrapper& operator=(allocator_wrapper&& rhs) noexcept {}
     allocator_wrapper(allocator_wrapper<T, AllocP> && rhs) noexcept : alloc_(std::move(rhs.alloc_)) {}
     template <typename U>
     allocator_wrapper(allocator_wrapper<U, AllocP> && rhs) noexcept : alloc_(std::move(rhs.alloc_)) {}
